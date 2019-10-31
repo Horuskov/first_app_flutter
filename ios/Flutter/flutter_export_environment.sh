f4501a2874af62668007f10c8a2df684b45ce2b5
Horuskov <horuskov@gmail.com>
2019-10-31 14:31:24 +0100

    Move the local folder to git

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/bbecue/Documents/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bbecue/Documents/Projects/gpi-techlab-social-app/gpi_socialapp_flutter"
+export "FLUTTER_TARGET=/Users/bbecue/Documents/Projects/gpi-techlab-social-app/gpi_socialapp_flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/bbecue/Documents/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
